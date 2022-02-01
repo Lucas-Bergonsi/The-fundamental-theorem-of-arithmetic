9297ea954190c41e83308f12d05cafff8b87a577
Lucas Bergonsi <98165189+Lucas-Bergonsi@users.noreply.github.com>
2022-02-01 16:47:22 -0300

    Update primos.f

--- primos.f
+++ primos.f
@@ -3,18 +3,18 @@
        Integer:: numero,p,numerofatores
        Integer, dimension(:), allocatable :: fatores
        Print*,'==============================================='
-       Print*, 'Entre com o n£mero que quer decompor:'
+       Print*, 'Entre com o n√∫mero que quer decompor:'
        Read*, numero
        allocate(fatores(numero))
 
-        p=2 !Valor do m¢dulo
+        p=2 !Valor do m√≥dulo
         numerofatores=1 !inicia a contagem de fatores
         do
-        if(mod(numero,p)==0) then !O n£mero Ç divisivel por 2,resto=0
+        if(mod(numero,p)==0) then !O n√∫mero ‚Äö divisivel por 2,resto=0
                fatores(numerofatores)=p
                 numerofatores= numerofatores+1
                numero=numero/p
-        else!Caso contr†rio, passa para o pr¢ximo divisor
+        else!Caso contr√°rio, passa para o pr√≥ximo divisor
                 p=p+1
              end if
         if(numero==1) then
@@ -23,7 +23,7 @@
             end if
         end do
            Print*,'==============================================='
-           Print*,' A decomposiá∆o Ç:'
+           Print*,' A decomposi√ß√£o ‚Äö:'
            Print*,numero, " = 1 ",(" x ",fatores(p),p=1,numerofatores)
            Print*,'==============================================='
 
